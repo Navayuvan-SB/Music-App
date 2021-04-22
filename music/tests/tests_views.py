21f98f7706e68cddd50975c683cf50e14e59af30
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 12:48:08 +0530

    Define testcases for Music Detail View

--- music/tests/tests_views.py
+++ music/tests/tests_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from music.models import MusicDirector, Music, Genre, Album, Label
 from django.urls import reverse
 
+
 class MusicListViewTest(TestCase):
 
     @classmethod
@@ -65,3 +66,76 @@ class MusicListViewTest(TestCase):
     def test_view_render_all_movies(self):
         response = self.client.get(reverse("musics"))
         self.assertEqual(len(response.context["music_list"]), 10)
+
+
+class MusicDetailViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+
+        music_director = MusicDirector.objects.create(
+            name="Sam Henry",
+            website="https://google.co.in",
+            date_of_birth="1980-06-11",
+            phone_number="9897927297"
+        )
+
+        label = Label.objects.create(
+            name="Sample Label Company",
+            founded_date="1989-03-12",
+            headquarters="USA"
+        )
+
+        number_of_genres = 2
+        for genre_id in range(number_of_genres):
+            Genre.objects.create(
+                title=f"Genre {genre_id}"
+            )
+        genres = Genre.objects.all()
+
+        album = Album.objects.create(
+            name="Album Name",
+            subtitle="A sample description",
+            music_director=music_director,
+            label=label,
+            asin="8574ASDASD",
+            release_date="2014-03-24"
+        )
+        album.genre.set(genres)
+        album.save()
+
+        number_of_musics = 3
+        for music_id in range(number_of_musics):
+            music = Music.objects.create(
+                name=f"Music {music_id}",
+                music_director=music_director,
+                duration="03:11",
+                album=album,
+                slug=f"music-{music_id}"
+            )
+
+            music.genre.set(genres)
+            music.save()
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get("/musics/music/music-1")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        response = self.client.get(
+            reverse("music-detail", kwargs={"slug": "music-1"})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(
+            reverse("music-detail", kwargs={"slug": "music-1"})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "music/music_detail.html")
+
+    def test_view_displays_correct_movie(self):
+        response = self.client.get(
+            reverse("music-detail", kwargs={"slug": "music-1"})
+        )
+        self.assertContains(response, "Music 1")
+
