981eb01bd9d8dbb6aff578b42f5dc0ce1ab3bb25
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 18:21:29 +0530

    Add Music to Inline for Admin model in admin panel

--- music/admin.py
+++ music/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Album, Genre, Label, Music, MusicDirector
 
 
+class MusicInline(admin.TabularInline):
+    model = Music
+
 @admin.register(Music)
 class MusicAdmin(admin.ModelAdmin):
     list_display = ("name", "duration", "music_director")
@@ -10,6 +13,7 @@ class MusicAdmin(admin.ModelAdmin):
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ("name", "subtitle", "music_director")
+    inlines=[MusicInline]
 
 
 @admin.register(Genre)
