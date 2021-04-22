84230b869ffdb7bf3de74e3c26ad02b7941ffffd
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 10:58:00 +0530

    Define Label model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Label(models.Model):
+
+    name = models.CharField(max_length=200, verbose_name="Label Company Name")
+    founded_date = models.DateField(verbose_name="Founded Date")
+    headquarters = models.CharField(max_length=512, verbose_name="Location of Headquarters")
+
