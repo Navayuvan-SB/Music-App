eb32f06242608f52813727a52c1ec8ad6bf5d8ba
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 12:37:21 +0530

    Add Music List view

--- music/views.py
+++ music/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Music
+
+
+class MusicListView(generic.ListView):
+    model = Music
