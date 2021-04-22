69d65e68b1bc7e56f635badf46da48dae43a1135
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 11:42:34 +0530

    Add all models to admin panel

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Album, Genre, Label, Music, MusicDirector
 
-# Register your models here.
+
+@admin.register(Music)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ("name", "duration", "music_director")
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("name", "subtitle", "music_director")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+
+@admin.register(MusicDirector)
+class MusicDirectorAdmin(admin.ModelAdmin):
+    list_display = ("name", "website", "date_of_birth")
+
+
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ("name", "founded_date", "headquarters")
