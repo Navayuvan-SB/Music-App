60ca79f2be137fcb62c4f10a2f7da2cf449172d8
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 10:59:34 +0530

    Define MusicDirector model

--- music/models.py
+++ music/models.py
@@ -6,3 +6,12 @@ class Label(models.Model):
     founded_date = models.DateField(verbose_name="Founded Date")
     headquarters = models.CharField(max_length=512, verbose_name="Location of Headquarters")
 
+
+class MusicDirector(models.Model):
+
+    name = models.CharField(max_length=200, verbose_name="Name of the music director")
+    website = models.URLField()
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=13)
+
+
