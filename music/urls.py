b25efcf91d236738b52b6fca19d6ac0a2d270881
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 12:57:43 +0530

    Add Album List View

--- music/urls.py
+++ music/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.MusicListView.as_view(), name="musics"),
     path('music/<slug:slug>', views.MusicDetailView.as_view(), name="music-detail"),
+    path('albums/', views.AlbumListView.as_view(), name="albums"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Music
+from .models import Music, Album
 
 
 class MusicListView(generic.ListView):
@@ -10,3 +10,7 @@ class MusicListView(generic.ListView):
 
 class MusicDetailView(generic.DetailView):
     model = Music
+
+
+class AlbumListView(generic.ListView):
+    model = Album
