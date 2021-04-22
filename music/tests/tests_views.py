a07de8b7b4e3c6b436835b68653ea2d068e127e1
navayuvansb <sb.navayuvan@gmail.com>
2021-04-22 13:02:02 +0530

    Define testcases for Album Detail View

--- music/tests/tests_views.py
+++ music/tests/tests_views.py
@@ -194,3 +194,68 @@ class AlbumListViewTest(TestCase):
     def test_view_render_all_movies(self):
         response = self.client.get(reverse("albums"))
         self.assertEqual(len(response.context["album_list"]), 10)
+
+
+
+class AlbumDetailViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        music_director = MusicDirector.objects.create(
+            name="Sam Henry",
+            website="https://google.co.in",
+            date_of_birth="1980-06-11",
+            phone_number="9897927297"
+        )
+
+        label = Label.objects.create(
+            name="Sample Label Company",
+            founded_date="1989-03-12",
+            headquarters="USA"
+        )
+
+        number_of_genres = 2
+        for genre_id in range(number_of_genres):
+            Genre.objects.create(
+                title=f"Genre {genre_id}"
+            )
+        genres = Genre.objects.all()
+
+        number_of_albums = 2
+        for album_id in range(number_of_albums):
+            album = Album.objects.create(
+                name=f"Album Name {album_id}",
+                subtitle="A sample description",
+                music_director=music_director,
+                label=label,
+                asin="8574ASDASD",
+                release_date="2014-03-24",
+                slug=f"album-{album_id}"
+            )
+            album.genre.set(genres)
+            album.save()
+
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get("/musics/albums/album-1")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        response = self.client.get(
+            reverse("album-detail", kwargs={"slug": "album-1"})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(
+            reverse("album-detail", kwargs={"slug": "album-1"})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "music/album_detail.html")
+
+    def test_view_displays_correct_movie(self):
+        response = self.client.get(
+            reverse("album-detail", kwargs={"slug": "album-1"})
+        )
+        self.assertContains(response, "Album Name 1")
